65ad63629a38fc1fd02d7070890d4b1b25315d82
Pham Hoai Nam <namph2102@gmail.com>
2023-11-17 07:22:36 +0700

    update env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,51 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  distDir: ".next",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "**",
+      },
+
+      {
+        protocol: "http",
+        hostname: "**",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+  experimental: {
+    serverActions: true,
+  },
+  env: {
+    DOMAIN_SERVER: process.env.DOMAIN_SERVER,
+  },
+};
 
 module.exports = nextConfig;

885f8fea9084be154d42a49eaa7ff7acf1749e3a
Pham Hoai Nam <namph2102@gmail.com>
2023-12-11 21:09:16 +0700

    update dasboard

--- app/bieu-do/page.tsx
+++ app/bieu-do/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import ChartFour from "@/components/Charts/ChartFour";
-import ChartOne from "@/components/Charts/ChartOne";
-import ChartThree from "@/components/Charts/ChartThree";
-import ChartTwo from "@/components/Charts/ChartTwo";
+// import ChartOne from "@/components/Charts/ChartOne";
+// import ChartThree from "@/components/Charts/ChartThree";
+// import ChartTwo from "@/components/Charts/ChartTwo";
 import { RootState } from "@/redux/store";
-import type { Metadata } from "next";
+
+import Head from "next/head";
 import { useSelector } from "react-redux";
 
 const ChartDashboard = () => {
@@ -13,7 +14,9 @@ const ChartDashboard = () => {
   return (
     <>
       <Breadcrumb pageName="Biểu đồ doanh thu" />
-
+      <Head>
+        <title>Biểu đồ</title>
+      </Head>
       <div className="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-7.5">
         <div className="col-span-12">{account.id && <ChartFour />}</div>
         {/* <ChartTwo /> */}
